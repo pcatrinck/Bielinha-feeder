41e6d9c26bfef1315e39cf72163a36ddbc971872
Patrick Catrinck <108846665+pcatrinck@users.noreply.github.com>
2023-10-02 00:46:15 -0300

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,62 @@
+#include <WiFi.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 16, 2);
+
+#define NTP_SERVER     "pool.ntp.org"
+#define UTC_OFFSET     -3 * 3600
+#define UTC_OFFSET_DST 0
+
+const char* ssid = "Wokwi-GUEST";
+const char* password =  "";
+
+void printLocalTime() {
+  struct tm timeinfo;
+
+  if (!getLocalTime(&timeinfo)) {
+    LCD.setCursor(0, 1);
+    LCD.println("Connection Err");
+    return;
+  }
+
+  LCD.setCursor(8, 0);
+  LCD.println(&timeinfo, "%H:%M:%S");
+  LCD.setCursor(0, 1);
+  LCD.println(&timeinfo, "%d/%m/%Y   %Z");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  LCD.init();
+  LCD.backlight();
+  LCD.setCursor(0, 0);
+  LCD.print("Connecting");
+  LCD.setCursor(0, 1);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    LCD.print(".");
+    delay(200);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  LCD.println("Online");
+  LCD.setCursor(0, 1);
+  LCD.println("Updating time...");
+
+  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+}
+
+void loop() {
+  printLocalTime();
+  delay(250);
+}
